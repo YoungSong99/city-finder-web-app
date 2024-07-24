0688746b43adac7bfd0c39d4e261e3a39702ce9d
young <hyangyoungs@gmail.com>
2024-07-24 11:09:29 -0500

    feat: update review controller and add views

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,66 +1,56 @@
 class ReviewsController < ApplicationController
+  before_action :set_city, only: [:new, :create, :index]
+  before_action :set_review, only: [:show, :edit, :update, :destroy]
+
   def index
-    matching_reviews = Review.all
-
-    @list_of_reviews = matching_reviews.order({ :created_at => :desc })
-
-    render({ :template => "reviews/index" })
+    @reviews = @city.reviews
   end
 
   def show
-    the_id = params.fetch("path_id")
+  end
 
-    matching_reviews = Review.where({ :id => the_id })
-
-    @the_review = matching_reviews.at(0)
-
-    render({ :template => "reviews/show" })
+  def new
+    @review = @city.reviews.new
   end
 
   def create
-    the_review = Review.new
-    the_review.user_id = params.fetch("query_user_id")
-    the_review.city_id = params.fetch("query_city_id")
-    the_review.pros = params.fetch("query_pros")
-    the_review.cons = params.fetch("query_cons")
-    the_review.recommend_family_type = params.fetch("query_recommend_family_type")
-    the_review.ratings = params.fetch("query_ratings")
-    the_review.zipcode = params.fetch("query_zipcode")
+    @review = @city.reviews.new(review_params)
+    @review.user = current_user
 
-    if the_review.valid?
-      the_review.save
-      redirect_to("/reviews", { :notice => "Review created successfully." })
+    if @review.save
+      redirect_to city_reviews_path(@city), notice: 'Review was successfully created.'
     else
-      redirect_to("/reviews", { :alert => the_review.errors.full_messages.to_sentence })
+      render :new
     end
   end
 
+  def edit
+  end
+
   def update
-    the_id = params.fetch("path_id")
-    the_review = Review.where({ :id => the_id }).at(0)
-
-    the_review.user_id = params.fetch("query_user_id")
-    the_review.city_id = params.fetch("query_city_id")
-    the_review.pros = params.fetch("query_pros")
-    the_review.cons = params.fetch("query_cons")
-    the_review.recommend_family_type = params.fetch("query_recommend_family_type")
-    the_review.ratings = params.fetch("query_ratings")
-    the_review.zipcode = params.fetch("query_zipcode")
-
-    if the_review.valid?
-      the_review.save
-      redirect_to("/reviews/#{the_review.id}", { :notice => "Review updated successfully."} )
+    if @review.update(review_params)
+      redirect_to city_review_path(@review.city, @review), notice: 'Review was successfully updated.'
     else
-      redirect_to("/reviews/#{the_review.id}", { :alert => the_review.errors.full_messages.to_sentence })
+      render :edit
     end
   end
 
   def destroy
-    the_id = params.fetch("path_id")
-    the_review = Review.where({ :id => the_id }).at(0)
+    @review.destroy
+    redirect_to city_reviews_path(@review.city), notice: 'Review was successfully destroyed.'
+  end
 
-    the_review.destroy
+  private
 
-    redirect_to("/reviews", { :notice => "Review deleted successfully."} )
+  def set_city
+    @city = City.find(params[:city_id])
+  end
+
+  def set_review
+    @review = Review.find(params[:id])
+  end
+
+  def review_params
+    params.require(:review).permit(:pros, :cons, :ratings, :recommend_family_type, :zipcode)
   end
 end
