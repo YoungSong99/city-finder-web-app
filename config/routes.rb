64bd5023af5dcd9bedc5775df04288fd53baa241
young <hyangyoungs@gmail.com>
2024-07-31 10:15:07 -0500

    feat: city detail

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root "dashboard#main"
+  root "dashboard#search"
   get 'dashboard', to: 'dashboard#index'
   post 'dashboard', to: 'dashboard#index'
   get 'search', to: 'dashboard#search', as: 'search_cities'
 
+  resources :cities, only: [:show]
   resources :cities do
     resources :reviews, only: [:new, :create, :index, :show]
   end
