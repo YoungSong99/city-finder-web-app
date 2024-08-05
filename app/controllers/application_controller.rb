970c6c5f1ffd071aa16f92b95352ad7112b11486
young <hyangyoungs@gmail.com>
2024-08-05 15:16:14 -0500

    feat: devise custom

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
   skip_forgery_protection
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
   def configure_permitted_parameters
