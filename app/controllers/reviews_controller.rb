321b29071760f8e0da7e90d7a92e866da02b4643
young <hyangyoungs@gmail.com>
2024-08-13 11:24:47 -0500

    update: review index open to non login user

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,5 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_city
   before_action :set_review, only: [:edit, :update, :destroy, :show]
 
