aad02b7a4d7300478e1ba42ffcfdea37c1583306
young <hyangyoungs@gmail.com>
2024-08-18 14:36:06 -0500

    fix thumbnail extension name error

--- app/services/meta_tag_service.rb
+++ app/services/meta_tag_service.rb
@@ -6,7 +6,7 @@ class MetaTagService
       description: "Discover your top 5 cities with just two quick questions. Ready to see how they stack up? Compare them all in a dynamic graph!",
       og: {
         title: "CityFinder::Find Your Perfect City",
-        image: ActionController::Base.helpers.asset_path('city_thumbnail.jpeg'),
+        image: ActionController::Base.helpers.asset_path('city_thumbnail.jpg'),
         description: "Discover your top 5 cities with just two quick questions. Ready to see how they stack up? Compare them all in a dynamic graph!",
         site_name: "CityFinder"
       }
