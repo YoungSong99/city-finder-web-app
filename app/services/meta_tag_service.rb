ede0d5cb46c27c42baf35af978d157e46200fb96
young <hyangyoungs@gmail.com>
2024-08-18 14:59:00 -0500

    feat: add new bg and thumbnail img

--- app/services/meta_tag_service.rb
+++ app/services/meta_tag_service.rb
@@ -2,11 +2,11 @@ class MetaTagService
   def self.defaults
     {
       site: "CityFinder::Find Your Perfect City",
-      image: ActionController::Base.helpers.asset_path('city_thumbnail.jpg'),
+      image: ActionController::Base.helpers.asset_path('city_thumbnail.png'),
       description: "Discover your top 5 cities with just two quick questions. Ready to see how they stack up? Compare them all in a dynamic graph!",
       og: {
         title: "CityFinder::Find Your Perfect City",
-        image: ActionController::Base.helpers.asset_path('city_thumbnail.jpg'),
+        image: ActionController::Base.helpers.asset_path('city_thumbnail.png'),
         description: "Discover your top 5 cities with just two quick questions. Ready to see how they stack up? Compare them all in a dynamic graph!",
         site_name: "CityFinder"
       }
