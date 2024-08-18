5010e817ae20fee151c81cbbce7105e6db375e6b
young <hyangyoungs@gmail.com>
2024-08-17 20:24:02 -0500

    feat: add mailtrap info

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,25 +1,17 @@
 class ContactsController < ApplicationController
   def new
-    @contact = Contact.new
   end
 
   def create
-    non_sensitive_data = params.require(:contact).permit(:category, :message)
+    @name = params[:contact_form][:name]
+    @email = params[:contact_form][:email]
+    @category = params[:contact_form][:category]
+    @message = params[:contact_form][:message]
 
-    session[:contact_data] = {
-      name: params[:contact][:name],
-      email: params[:contact][:email]
-    }
+    NotifierMailer.simple_message(@name, @message, @email, @category).deliver_now
 
-    @contact = Contact.new(non_sensitive_data.merge(session[:contact_data]))
 
-    if @contact.save
-      session.delete(:contact_data)
-      flash[:notice] = "We're excited to hear from you. Let's keep making CityFinder even better together!"
-      redirect_to new_contact_path
-    else
-      flash[:alert] = "Oh no! Something went wrong with your message. Please give it another go—we're here to help you find the perfect city!"
-      render :new
-    end
+    flash[:notice] = "We're excited to hear from you. Let's keep making CityFinder even better together!"
+    redirect_to new_contact_path
   end
 end
