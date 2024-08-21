2b8251178624f5a372d6c10f872c07c1bc14a811
young <hyangyoungs@gmail.com>
2024-08-21 02:19:50 -0500

    update: to show custom page

--- config/application.rb
+++ config/application.rb
@@ -37,6 +37,6 @@ module RailsTemplate
 
     # Custom 404, 500
     config.exceptions_app = self.routes
-    config.action_dispatch.show_exceptions = false
+    config.action_dispatch.show_exceptions = :all
   end
 end
